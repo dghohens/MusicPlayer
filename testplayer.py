043efab5aed333c6efcfa9644b4bac259ee2b691
Donald Hohenstein <dghohens@gmail.com>
2019-01-05 21:38:19 -0500

    Day 66 of 100 days of code, working on music player.

--- testplayer.py
+++ testplayer.py
@@ -3,7 +3,28 @@
 
 from playsound import playsound
 
-file = '<filename>'
+from ctypes import c_buffer, windll
+from sys import getfilesystemencoding
 
-playsound(file)
+file = 'K:\\Music\\Hotline Miami 2 OST\\Soundtrack\\49 Escape From Midwitch Valley.mp3'
 
+#playsound(file)
+
+
+def winCommand(*command):
+    buf = c_buffer(255)
+    command = ' '.join(command).encode(getfilesystemencoding())
+    errorCode = int(windll.winmm.mciSendStringA(command, buf, 254, 0))
+    if errorCode:
+        errorBuffer = c_buffer(255)
+        windll.winmm.mciGetErrorStringA(errorCode, errorBuffer, 254)
+        exceptionMessage = ('\n    Error ' + str(errorCode) + ' for command:'
+                                                              '\n        ' + command.decode() +
+                            '\n    ' + errorBuffer.value.decode())
+        raise errorCodeException(exceptionMessage)
+    return buf.value
+
+durationInMS = winCommand('status', file, 'length')
+print(durationInMS)
+
+#playsound(file)
