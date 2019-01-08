788128d96fae98e3be3f4e0d1a284f9f139b0386
dghohens <dghohens@gmail.com>
2019-01-08 17:09:01 -0500

    Day 69 of 100 days of code, worked on interface.

--- InterfaceTest.py
+++ InterfaceTest.py
@@ -1,12 +1,20 @@
 from colorama import init, Fore, Back, Style
-import os
+import os, configparser
 
 init()
 
-session_width = 80
-session_height = 25
+config = configparser.ConfigParser()
+config.read('playerConfig')
 
-file = '<filename>'
+session_width = int(config['Interface sizing']['Session width'])
+session_height = int(config['Interface sizing']['Session height'])
+midwidth = (session_width - 6)//2
+
+background = config['Interface colors']['Background']
+file_fore = config['Interface colors']['File foreground']
+file_select = config['Interface colors']['File selection']
+
+parent_directory = config['Directories']['Base directory']
 
 def abbrev_list(inlist):
     # Abbreviate list if it's too long
@@ -23,41 +31,49 @@ def abbrev_list(inlist):
     return outlist
 
 
+def abbrev_file(infile):
+    global midwidth
+    if len(infile) > midwidth:
+        outfile = infile[:midwidth - 3] + '...'
+    else:
+        outfile = infile
+    return outfile
+
+
 def player_window():
     print(Fore.YELLOW, '')
-    print(Back.BLACK, '')
+    print(Back.background, '')
     print('┌=[' + '    Artist name - Album name    ' + ']=┐ ┌───[' + '   Artist name - Song name   ' + ']───┐', end = '')
     print(('\n' + '│' + ' ' * 36 + '│ │' + ' ' * 37 + '│') * 22)
     print('\033[F' + '└' + '─' * 36 + '┘ └' + '─' * 37 + '┘')
 
 
 def file_window(subdirs, selected_subdirs):
-    midwidth = (session_width - 6)//2
-    print(Fore.GREEN, '')
-    print(Back.BLACK, '')
-    print('┌=[' + '     Current Folder Contents    ' + ']=┐ ┌───[' + '  Selected Folder Contents   ' + ']───┐', end='')
+    global file_fore, background, midwidth
+    print(file_fore, '')
+    print(background, '')
+    print('┌=[' + '{:^{midwidth}}'.format('Parent Folder Contents', midwidth = midwidth - 4) + ']=┐  ┌──[' + '{:^{midwidth}}'.format('Selected Folder Contents', midwidth = midwidth - 6) + ']──┐', end='')
     for i in range(session_height - 2):
         c = abbrev_list(subdirs)
         d = abbrev_list(selected_subdirs)
-        a = c[i]
-        b = d[i]
-        if len(a) > 36:
-            a = a[0:32] + '...'
-        if len(b) > 36:
-            b = b[0:32] + '...'
+        a = abbrev_file(c[i])
+        b = abbrev_file(d[i])
         # https://stackoverflow.com/questions/29044940/how-can-you-use-a-variable-name-inside-a-python-format-specifier
         print('\n' +'│' + '{:{midwidth}}'.format(a, midwidth = midwidth) + '│  │' + '{:{midwidth}}'.format(b, midwidth = midwidth) + '│', end = '')
         # print('\n' +'│' + '{:{}}'.format(a, midwidth) + '│ │' + '{:{}}'.format(b, midwidth) + '│', end = '')
     #print(('\n' + '│' + ' ' * 36 + '│ │' + ' ' * 37 + '│') * 22)
     #print('\033[F' + '└' + '─' * 36 + '┘ └' + '─' * 37 + '┘')
-    print('\n' + '└' + '─' * 36 + '┘ └' + '─' * 37 + '┘')
+    print('\n' + '└' + '─' * midwidth + '┘  └' + '─' * midwidth + '┘')
 
-def get_dir(current_working_directory, selected_directory):
+def get_dir(current_working_directory, selected_directory = None):
     subdirs = os.listdir(current_working_directory)
+    if selected_directory == None:
+        selected_directory = current_working_directory + subdirs[0]
     selected_subdirs = os.listdir(selected_directory)
     return subdirs, selected_subdirs
 
-dirs = get_dir('<parentdir>', '<subdir>')
+dirs = get_dir(parent_directory)
 
-print(len(dirs[0]), len(dirs[1]))
 file_window(dirs[0], dirs[1])
+
+# When a key is pressed, clear screen and redraw https://stackoverflow.com/questions/2084508/clear-terminal-in-python
